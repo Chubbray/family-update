72a9a9a4e74d3e25348215c602978321d89d9e8b
Chubbray <chubbray1@gmail.com>
2020-09-11 16:59:16 -0600

    cleaning up api-users.js

--- routes/api-users.js
+++ routes/api-users.js
@@ -1,12 +1,8 @@
 const router = require("express").Router();
 const { User, Thought, Reaction } = require("/models");
 
-// The `/api/categories` endpoint
 
 router.get("/", (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
-
   User.findAll({
     include: [Thought, Reaction],
   })
@@ -18,16 +14,11 @@ router.get("/", (req, res) => {
 });
 
 router.get("/:id", (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
-
   User.findOne({
     where: {
       id: req.params.id
     },
-
     include: [Thought, Reaction]
-
   })
     .then(dbUserData => res.json(dbUserData))
     .catch(err => {
@@ -37,13 +28,10 @@ router.get("/:id", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-  // create a new category
-
   User.create(
     req.body
   )
     .then(dbUserData => res.status(200).json(dbUserData))
-
     .catch(err => {
       console.log(err);
       res.status(400).json(err);
@@ -51,8 +39,6 @@ router.post("/", (req, res) => {
 });
 
 router.put("/:id", (req, res) => {
-  // update a category by its `id` value
-
   User.update(req.body,
     {
       where: {
@@ -67,15 +53,12 @@ router.put("/:id", (req, res) => {
 });
 
 router.delete("/:id", (req, res) => {
-  // delete a category by its `id` value
-
   User.destroy({
     where: {
       id: req.params.id
     }
   })
     .then(dbUserData => res.status(200).json(dbUserData))
-
     .catch(err => {
       console.log(err);
       res.status(400).json(err);
