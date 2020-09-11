31a462fbc3903a51eb06e7720e6553ca0b9ec11d
Chubbray <chubbray1@gmail.com>
2020-09-11 02:51:35 -0600

    updated the readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 *Family-Update
+
+I first got my repository set up, then I moved on to adding the README and the .gitignore. Once that was completed and saved, I added the models and routes folders. I then added in the index.js, reaction.js, thought.js, and user.js files. Then I put in the code for index.js and got started on user.js.
--- models/User.js
+++ models/User.js
@@ -11,7 +11,7 @@ const UserSchema = new Schema({
         type: String,
         required: "Email address is required",
         unique: true,
-        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, "Please fill a valid email address"],
     },
     thoughts: [
         {
